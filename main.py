d40e9be3716cdffb084df5c9161818c939d27f7e
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2023-07-27 21:58:47 +0200

    Update dependencies and better error handling

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message.text == "/start":
         await update.message.reply_text("Let's download some weejios")
         return
-    await context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
+    await context.bot.send_chat_action(
+        chat_id=update.effective_message.chat_id, action=ChatAction.TYPING
+    )
     ydl_opts = {
-        'outtmpl': '%(id)s.%(ext)s',
+        "outtmpl": "%(id)s.%(ext)s",
     }
     video_url = update.message.text
     if not video_url.startswith("https://"):
@@ -24,20 +26,23 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             info = ydl.extract_info(
                 video_url,
             )
-        except youtube_dl.utils.DownloadError:
-            await update.message.reply_text(text=f"I could not download this 😅")
-        output_file = f'{info.get("id")}.{info.get("ext")}'
-        try:
-            await update.message.reply_video(video=open(output_file, 'rb'))
-        except:
-            await update.message.reply_text(
-                text=f"Looks like something went wrong uploading the video to telegram. It possibly is bigger than 50Mb.")
-        os.remove(output_file)
+            output_file = f'{info.get("id")}.{info.get("ext")}'
+            try:
+                await update.message.reply_video(video=open(output_file, "rb"))
+            except:
+                await update.message.reply_text(
+                    text="Looks like something went wrong uploading the video to telegram. It possibly is bigger than "
+                    "50Mb."
+                )
+            os.remove(output_file)
+        except youtube_dl.utils.DownloadError as error:
+            print(error)
+            await update.message.reply_text(text="I could not download this 😅")
 
 
-if __name__ == '__main__':
-    TOKEN = os.environ.get('TELEGRAM_TOKEN')
-    WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
+if __name__ == "__main__":
+    TOKEN = os.environ.get("TELEGRAM_TOKEN")
+    WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(MessageHandler(callback=download, filters=ALL))
-    app.run_webhook(listen='0.0.0.0', port=9991, webhook_url=WEBHOOK_URL)
+    app.run_webhook(listen="0.0.0.0", port=9991, webhook_url=WEBHOOK_URL)
